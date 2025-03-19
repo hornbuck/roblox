16dcfa58f20eb559523e3e3200d3d2fb5a4b3234
Kaitlyn Hornbuckle <124624417+hornbuck@users.noreply.github.com>
2025-03-19 00:30:07 -0700

    Add files via upload

--- forge.config.js
+++ forge.config.js
@@ -4,7 +4,13 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
-    icon: 'roblox_phippy.ico'
+    icon: './roblox_phippy',
+    ignore: [
+      /node_modules/,
+      /test/,
+      /out/,
+      /\.git/
+    ]
   },
   rebuildConfig: {},
   makers: [
@@ -14,9 +20,9 @@ module.exports = {
         name: 'LondonInRoblox', // Internal name for the app (used in the installer)
         authors: 'CNCF', // The name of the developer/company
         description: 'Retrieves the newest Roblox file for KubeCon Kids Day 2025',
-        setupIcon: 'roblox_phippy.ico', // Path to your icon for the installer
+        setupIcon: './roblox_phippy.ico', // Path to your icon for the installer
         setupExe: 'UpdateLondon.exe', // Custom name for the generated installer
-        noMsi: true, // If true, prevents creating an MSI installer
+        noMsi: false, // If true, prevents creating an MSI installer
       },
     },
   ],
